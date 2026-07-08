0e1cbb5dd9ef4a7fe5873a607869f5033875118c
Shashwat <shashwatshukla0228@gmail.com>
2026-07-08 19:03:29 +0530

    Build Resource model

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -3,8 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Resource extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that can be mass assigned.
+     */
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'title',
+        'slug',
+        'type',
+        'source_url',
+        'description',
+        'content',
+        'status',
+        'is_favorite',
+    ];
+
+    /**
+     * Attribute casting.
+     */
+    protected $casts = [
+        'is_favorite' => 'boolean',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
 }
